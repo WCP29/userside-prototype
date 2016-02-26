6cb27a3d1aa7ede5c11f21ebd55baac528368cf1
grinbergjeff <grinbergjeff@gmail.com>
2016-02-26 02:27:48 +0000

    Implemented the 'Campus Map Base Image' section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	console.log('jQuery running properly');
 
-	$('#where-am-i, #find-a-room, #navigation, #footer, #begin-menu').hide();
+	$('#where-am-i, #find-a-room, #navigation, #footer, #begin-menu, #campus-map').hide();
 	startPress();
 	whereAmI();
 	findRoom();
 	navigationSectionShow();
+	campusMapShow();
 
 
 
@@ -13,7 +14,7 @@ $(document).ready(function() {
 
 function startPress() {
 	$('#begin-button').on('click', function() {
-		$('#where-am-i,#find-a-room, #navigation, #footer').hide();
+		$('#where-am-i,#find-a-room, #navigation, #footer, #campus-map').hide();
 		$('#begin-menu').fadeIn('medium');
 	});
 };
@@ -22,22 +23,30 @@ function startPress() {
 function whereAmI() {
 	$('#smenuc-where, #side-where, #where-link, #side-link3').on('click', function() {
 		console.log('where clicked.');
-		$('#find-a-room, #navigation, #footer').hide();
+		$('#find-a-room, #navigation, #footer, #campus-map').hide();
 		$('#where-am-i').fadeIn('medium');
 	});
 };
 
 function findRoom() {
 	$('#smenuc-find, #side-find, #find-link, #side-link4').on('click', function() {
-		$('#where-am-i, #navigation, #footer').hide();
+		$('#where-am-i, #navigation, #footer, #campus-map').hide();
 		$('#find-a-room').fadeIn('medium');
 	});
 };
 
 function navigationSectionShow() {
 	$('#smenuc-navig, #side-nav, #navig-link, #side-link5').on('click', function() {
-		$('#where-am-i, #find-a-room').hide();
-		$('#navigation, #footer').fadeIn('medium');
+		$('#where-am-i, #find-a-room, #campus-map').hide();
+		$('#navigation').fadeIn('medium');
+
+	});
+};
+
+function campusMapShow() {
+	$('#smenu-campmap, #side-map, #navig-link, #side-link6').on('click', function() {
+		$('#where-am-i, #find-a-room, #navigation').hide();
+		$('#campus-map, #footer').fadeIn('medium');
 
 	});
 };
